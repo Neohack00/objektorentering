fb9d7420e9cb490b5fe893281531d33a39bdd28b
Neohack00 <neo@bylock.se>
2019-09-20 09:56:21 +0200

    Class Car created and project Car2 created

--- Demo T40920/Program.cs
+++ Demo T40920/Program.cs
@@ -10,6 +10,24 @@ namespace Demo_T40920
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Creating Car, stand by");
+            Car c;
+            c = new Car();
+            c.RegistrationNumber = "ABC123";
+            c.HorsePower = 170;
+            c.Color = "Black";
+            Console.WriteLine("Car created sucessfully");
+            Console.ReadLine();
+            Console.WriteLine("We have a {0} car with registation {1} and {2}",
+                c.Color, c.RegistrationNumber, c.HorsePower);
+            Console.ReadLine();
+
         }
     }
+    class Car
+    {
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public int HorsePower { get; set; }
+    }   
 }
